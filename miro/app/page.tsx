80d657f83fce738ee7e2e80c757b8fe22cda0023
harshraj6582 <harshraj211430@gmail.com>
2024-02-12 19:45:37 +0530

    Convex and Clerk Setup

--- miro/app/page.tsx
+++ miro/app/page.tsx
@@ -1,6 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <Button  size = "sm" >Click ME </Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        This is a screen for authenticated users only 
+      </div>
+      <div>
+        <UserButton/>
+      </div>
+
+    </div>
   );
 }
